c807d327312acf6238150da11bfc9379d7ab6a3f
Jonathan Reyes <79552541+J0nRey@users.noreply.github.com>
2022-07-22 20:28:22 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,78 @@
+//window.alert()
+alert("hola mundo")
+console.log("hola mundo")
+
+
+var variable1 = "jonathan rees alatorre"
+console.log(typeof variable1)
+
+var variable2 = 27
+console.log(typeof variable2)
+
+var variable3= {name:"jonathan Reyes Altorre"}
+console.log(typeof variable3)
+
+var variable4 = false
+console.log(typeof variable4)
+
+var variable5;
+console.log(typeof variable5)
+console.log(variable5)
+
+var variable6 = null
+console.log(typeof variable6)
+console.log(variable6)
+
+var variable7 = [
+    "4",
+    27,
+    {name:"jonathan"}
+]
+console.log(typeof variable7)
+console.log(variable7)
+
+var name = "jonathan"
+var lastName = "Reyes Alatorre"
+
+console.log(name, lastName)
+
+
+var num1 = 5
+var num2 = 5
+
+console.log( num1 + num2)
+console.log( num1 - num2)
+console.log( num1 * num2)
+console.log( num1 / num2)
+console.log( num1 % num2)
+console.log( ++ num1)
+console.log( -- num2)
+
+
+var AdditionResult= num1+ num2
+
+document.write("<h1>"+name+" "+lastName+"</h1>")
+
+document.write("<h1>"+AdditionResult+" "+num1+num2+"</h1>")
+
+document.write("<h1>"+(num1+num2)+" "+num1+num2+"</h1>")
+
+// Lo escribe tantas veces se escriba
+
+document.write("<h1>hola</h1>")
+document.write("<p>hola</p>")
+document.write("<p>hola</p>")
+document.write("<p>hola</p>")
+document.write("<p>hola</p>")
+document.write("<p>hola</p>")
+document.write("<p>hola</p>")
+
+// Sobrescribe tantas veces se escriba
+
+document.writeln("<h1>hola</h1>")
+document.writeln("<p>hola</p>")
+document.writeln("<p>hola</p>")
+document.writeln("<p>hola</p>")
+document.writeln("<p>hola</p>")
+document.writeln("<p>hola</p>")
+document.writeln("<p>hola</p>")
